104f8a8a510a1143bce4fec4b5e01f211a655108
Unknown <unknown@example.com>
2019-10-05 18:00:34 +0200

    add comments
    added comments on html and js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import './styles.scss'; // entry point for styles
 import $ from 'jquery';
-import tab from 'jquery-ui/ui/widgets/tabs';
-import sort from 'jquery-ui/ui/widgets/sortable';
-import { test } from './app/main'; // example of an ES6 import
+// import tab from 'jquery-ui/ui/widgets/tabs';
+// import sort from 'jquery-ui/ui/widgets/sortable';
 import { rand } from './app/helpers';
 
-console.log(test); // should display "it works!" in console on launch
+/******************** JETON ********************/
 
 $('.case').on('click', function () {
     $(this).append('<div class="jeton">1€</div>');
+    
+    $('button').on('click', function () {
+    $('.jeton').remove();
+});
 });
 
+/******************** NOMBRE ALEATOIRE ********************/
 
 $('button').on('click', function () {
     const randomNb = rand(0, 36);
     $('.random').text(randomNb);
-    console.log(randomNb);
+    // console.log(randomNb);
+    // console.log($(".case").find('.jeton').length);
 
-    console.log($(".case").find('.jeton').length);
-
-    $('jeton').remove();
 });
 
 
